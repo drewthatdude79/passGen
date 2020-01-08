f3fa9d351d517a30b1a28c9298d974d6a7bce864
Andrew Bolton <drupy19@yahoo.com>
2020-01-07 23:05:53 -0500

    update js pass gen

--- script.js
+++ script.js
@@ -4,4 +4,10 @@ var lowerCase = "abcdefghijklmnopqrstuvwxyz";
 var nums = "0123456789";
 var syms = "!@#$%^&*()+=?<>";
 
-//
+var password="";
+
+var upperCase = confirm("includes upper case");
+var lowerCase = confirm("includes lower case");
+var nums = confirm("includes nums");
+var syms = confirm("includes syms");
+
