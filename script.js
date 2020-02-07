9b46a0bfaea534b4e1bb8f6ddd6327636ddcc556
Andrew Bolton <drupy19@yahoo.com>
2020-02-07 11:03:50 -0500

    Confirm is updated

--- script.js
+++ script.js
@@ -1,38 +1,50 @@
 // set the variables
-function generate() {
+function generator() {
+
+    var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+    var nums = "0123456789";
+    var syms = "!@#$%^&*()+=?<>,./";
+    var totalLength = 0;
     
-var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-var lowerCase = "abcdefghijklmnopqrstuvwxyz";
-var nums = "0123456789";
-var syms = "!@#$%^&*()+=?<>";
+    var password = "";
 
-var password="";
+    var rand = "random generated password";
 
-var upperCase = confirm("includes upper case");
-var lowerCase = confirm("includes lower case");
-var nums = confirm("includes nums");
-var syms = confirm("includes syms");
-var totalLength = promt("password to be 8 to 128 characters");
+    // var hasUpperCase = confirm("includes upper case");
+    // var hasLowerCase = confirm("includes lower case");
+    // var hasNums = confirm("includes nums");
+    // var hasSyms = confirm("includes syms");
+    // var hasTotalLength = prompt("password to be 8 to 128 characters");
 
+    // alert("includes upper case: " + hasUpperCase);
+    // alert("includes lower case" + hasLowerCase);
+    // alert("includes nums" + hasNums);
+    // alert("includes syms" + hasSyms);
 
-for(var i = 0; i < totalLength; i++) {
-    if(upperCase) {
-        var rand = getRandomChar(upperCase)
-        password += rand;
+    var genPass = Math.floor(Math.random() * 10) + 1;
+
+    for (var i = 0; i < totalLength; i++) {
+        if (upperCase) {
+            var rand = getRandomChar(upperCase)
+            password += rand;
+        }
+
+        if (lowerCase) {
+            var rand = getRandomChar(lowerCase)
+            password += rand;
+        }
+
+        if (nums) {
+            var rand = getRandomChar(nums)
+            password += rand;
+        }
+
+        if (syms) {
+            var rand = getRandomChar(syms)
+            password += rand;
+        }
+
     }
-
-    if(lowerCase) {
-        var rand = getRandomChar(lowerCase)
-        password += rand;
-    }
-
-    if(nums) {
-        var rand = getRandomChar(nums)
-        password += rand;
-    }
-
-    if(syms) {
-        var rand = getRandomChar(syms)
-        password += rand;
-    }
-}};
+}
+generator()
